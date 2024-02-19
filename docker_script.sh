d161cadb810c92ccf2855d9f7cf4d5de5420646e
Pratik Gaikwad <pratikgaikwad1004@gmail.com>
2024-02-20 00:38:32 +0530

    Script changed for existing image

--- docker_script.sh
+++ docker_script.sh
@@ -1,4 +1,14 @@
 echo "****************************************************************"
+echo "Removing the existing docker container for the popcorn-data-server"
+echo "****************************************************************"
+docker rm -f popcorn-data-server
+
+echo "****************************************************************"
+echo "Removing the existing docker image for the popcorn-data-server"
+echo "****************************************************************"
+docker rmi -f popcorn-data-server
+
+echo "****************************************************************"
 echo "Building the docker image for the popcorn-data-server"
 echo "****************************************************************"
 docker build -t popcorn-data-server .
@@ -6,7 +16,7 @@ docker build -t popcorn-data-server .
 echo "****************************************************************"
 echo "Running the docker container for the popcorn-data-server"
 echo "****************************************************************"
-docker run -d -p 8000:8000 -v popcorn_images:/app/images -v popcorn_videos:/app/videos --name popcorn-data-server popcorn-data-server
+docker run -d -p 8000:8000 -v popcorn_images:/app/images -v popcorn_videos:/app/videos --name popcorn-data-server --rm popcorn-data-server
 
 echo "****************************************************************"
 echo "Docker container for the popcorn-data-server is running"
