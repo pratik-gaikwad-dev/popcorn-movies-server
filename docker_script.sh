df63cffdb9c8390db0fbb81e0646eeb1234c71f1
Pratik Gaikwad <pratikgaikwad1004@gmail.com>
2024-02-20 00:30:03 +0530

    Docker volumes created

--- docker_script.sh
+++ docker_script.sh
@@ -6,7 +6,7 @@ docker build -t popcorn-data-server .
 echo "****************************************************************"
 echo "Running the docker container for the popcorn-data-server"
 echo "****************************************************************"
-docker run -d -p 8000:8000 --name popcorn-data-server popcorn-data-server
+docker run -d -p 8000:8000 -v popcorn_images:/app/images -v popcorn_videos:/app/videos --name popcorn-data-server popcorn-data-server
 
 echo "****************************************************************"
 echo "Docker container for the popcorn-data-server is running"
