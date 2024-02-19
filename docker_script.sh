d6a0f3d78a9266de2289af21050243a61a641ed5
Pratik Gaikwad <pratikgaikwad1004@gmail.com>
2024-02-19 23:39:15 +0530

    Docker script modified to create image and start server

---
+++ docker_script.sh
@@ -0,0 +1,13 @@
+echo "****************************************************************"
+echo "Building the docker image for the popcorn-data-server"
+echo "****************************************************************"
+docker build -t popcorn-data-server .
+
+echo "****************************************************************"
+echo "Running the docker container for the popcorn-data-server"
+echo "****************************************************************"
+docker run -d -p 80:8000 --name popcorn-data-server popcorn-data-server
+
+echo "****************************************************************"
+echo "Docker container for the popcorn-data-server is running"
+echo "****************************************************************"
