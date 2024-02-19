ff64cfdabf013302e10067397c3fead31c163452
Pratik Gaikwad <pratikgaikwad1004@gmail.com>
2024-02-19 23:47:59 +0530

    Ports changed

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ COPY . /app
 RUN npm install && \
     npm install pm2 -g
 
-EXPOSE 3000
+EXPOSE 8000
 
 CMD ["pm2-runtime", "index.js"]
--- docker_script.sh
+++ docker_script.sh
@@ -6,7 +6,7 @@ docker build -t popcorn-data-server .
 echo "****************************************************************"
 echo "Running the docker container for the popcorn-data-server"
 echo "****************************************************************"
-docker run -d -p 80:8000 --name popcorn-data-server popcorn-data-server
+docker run -d -p 8000:8000 --name popcorn-data-server popcorn-data-server
 
 echo "****************************************************************"
 echo "Docker container for the popcorn-data-server is running"
