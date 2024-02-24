b2201acbdad467902fd8c6b24082ca609397c939
Pratik Gaikwad <pratikgaikwad1004@gmail.com>
2024-02-24 13:47:52 +0530

    test

--- docker_script.sh
+++ docker_script.sh
@@ -24,11 +24,20 @@ docker tag $IMAGE:$BUILD_ID pratikgaikwad/$IMAGE:$BUILD_ID
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push pratikgaikwad/$IMAGE:$BUILD_ID
 
+echo "****************************************************************"
+echo "**************** Stoping previous containers *******************"
+echo "****************************************************************"
+docker stop popocorn-data
+
+echo "****************************************************************"
+echo "**************** Removing previous image *******************"
+echo "****************************************************************"
+docker image prune -a -f
 
 echo "****************************************************************"
 echo "*********************** Deploying Image ************************"
 echo "****************************************************************"
-docker run -d -p 8000:8000 -v popcorn_popcorn_images:/app/images -v popcorn_popcorn_videos:/app/videos --network popcorn_network --name popocorn-data pratikgaikwad/$IMAGE:$BUILD_ID
+docker run -d -p 8000:8000 --rm -v popcorn_popcorn_images:/app/images -v popcorn_popcorn_videos:/app/videos --network popcorn_network --name popocorn-data pratikgaikwad/$IMAGE:$BUILD_ID
 
 
 
