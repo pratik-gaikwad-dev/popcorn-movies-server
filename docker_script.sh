a6db5965368fb3bab07d1a0d553e620944917148
Pratik Gaikwad <pratikgaikwad1004@gmail.com>
2024-02-24 13:29:17 +0530

    test

--- docker_script.sh
+++ docker_script.sh
@@ -2,15 +2,15 @@
 
 IMAGE=$IMAGE_NAME
 
-echo "****************************************************************"
-echo "Removing the existing docker container for the popcorn-data-server"
-echo "****************************************************************"
-docker rm -f $IMAGE
+# echo "****************************************************************"
+# echo "Removing the existing docker container for the popcorn-data-server"
+# echo "****************************************************************"
+# docker rm -f $IMAGE
 
-echo "****************************************************************"
-echo "Removing the existing docker image for the popcorn-data-server"
-echo "****************************************************************"
-docker rmi -f $IMAGE
+# echo "****************************************************************"
+# echo "Removing the existing docker image for the popcorn-data-server"
+# echo "****************************************************************"
+# docker rmi -f $IMAGE
 
 echo "****************************************************************"
 echo "Building the docker image for the popcorn-data-server"
@@ -20,17 +20,9 @@ docker build -t $IMAGE .
 echo "****************************************************************"
 echo "PUSHING IMAGE TO DOCKERHUB"
 echo "****************************************************************"
-docker tag $IMAGE:latest pratikgaikwad/$IMAGE:latest
+docker tag $IMAGE:$BUILD_ID pratikgaikwad/$IMAGE:$BUILD_ID
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push pratikgaikwad/$IMAGE
 
-echo "****************************************************************"
-echo "Restarting docker compose"
-echo "****************************************************************"
-cd /var/jenkins_home/popcorn
 
-docker compose down
-docker rmi $IMAGE
-docker compose pull
-docker compose up --force-recreate --build -d
-docker image prune -f
+
