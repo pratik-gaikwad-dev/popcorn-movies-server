462c8eb68bd45ffbd8d898c2b896dbbf7b1ac5aa
Pratik Gaikwad <pratikgaikwad1004@gmail.com>
2024-02-24 13:38:32 +0530

    test

--- docker_script.sh
+++ docker_script.sh
@@ -13,16 +13,23 @@ IMAGE=$IMAGE_NAME
 # docker rmi -f $IMAGE
 
 echo "****************************************************************"
-echo "Building the docker image for the popcorn-data-server"
+echo "***** Building the docker image for the popcorn-data-server ****"
 echo "****************************************************************"
 docker build -t $IMAGE:$BUILD_ID .
 
 echo "****************************************************************"
-echo "PUSHING IMAGE TO DOCKERHUB"
+echo "**************** PUSHING IMAGE TO DOCKERHUB ********************"
 echo "****************************************************************"
 docker tag $IMAGE:$BUILD_ID pratikgaikwad/$IMAGE:$BUILD_ID
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push pratikgaikwad/$IMAGE:$BUILD_ID
+
+
+echo "****************************************************************"
+echo "*********************** Deploying Image ************************"
+echo "****************************************************************"
+docker run -d -p 8000:8000 -v popcorn_popcorn_images:/app/images -v popcorn_popcorn_videos:/app/videos --network popcorn_network --name popocorn-data pratikgaikwad/$IMAGE:$BUILD_ID
+
 
 
 
